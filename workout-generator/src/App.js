4cf0f91f1a7f9b84e4679dc51a99078e01aed0b7
Zach Nicoll <zaxternoob@gmail.com>
2020-02-10 12:11:06 +1000

    Randomly generates X-number of upper or lower body workouts.

--- workout-generator/src/App.js
+++ workout-generator/src/App.js
@@ -1,49 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import styles from './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import styles from "./App.css";
 
+// Button Component //
 export const Button = class Button extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            text:this.props.text,
-            func:this.props.func,
-            clicked:false
-        };
-    }
-    
-    click = async () => {
-            await this.setState({clicked:true}, function(){
-                this.state.func(this.state.clicked);
-            });
-            await this.setState({clicked:false});
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: this.props.text,
+      func: this.props.func,
+      clicked: false
+    };
+  }
 
-    render() {
-        return(
-            <button onClick={this.click}>{this.state.text}</button>
-        );
-    }
-}
+  click = async () => {
+    await this.setState({ clicked: true }, function() {
+      this.state.func(this.state.clicked);
+    });
+    await this.setState({ clicked: false });
+  };
 
+  render() {
+    return <button onClick={this.click}>{this.state.text}</button>;
+  }
+};
+///////////////////
+
+// Label Component //
 export const Label = class Label extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-        
-        };
-    }
-    
-    render() {
-        return(
-            <div class="item">
-                <a href={this.props.link}>
-                    <p><b>{this.props.name}</b></p>
-                    <p>Type: {this.props.type}</p>
-                    <p>Target Muscle: {this.props.targetmuscle}</p>
-                    <p>Difficulty: {this.props.difficulty}</p>
-                </a>
-            </div>
-        );
-    }
-}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div class="item">
+        <a href={this.props.link}>
+          <p>
+            <b>{this.props.name}</b>
+          </p>
+          <hr></hr>
+          <p>Type: {this.props.type}</p>
+          <p>Target Muscle: {this.props.targetmuscle}</p>
+          <p>Difficulty: {this.props.difficulty}</p>
+        </a>
+      </div>
+    );
+  }
+};
+///////////////////
+
+// Form Component //
+export const Form = class Class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Number of Exercises:{"  "}
+        <input type="text" name="numExercises" onChange={event => this.props.numFunc(event.target.value)}></input> <br></br>
+        Type:{"  "}
+        <select onChange={event => this.props.typeFunc(event.target.value)}>
+          <option value="Upper Body">Upper Body</option>
+          <option value="Lower Body">Lower Body</option>
+        </select>
+        <br></br>
+        <br></br>
+      </div>
+    );
+  }
+};
+///////////////////
--- workout-generator/src/index.js
+++ workout-generator/src/index.js
@@ -1,43 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Button, Label} from './App.js'
-import Workouts from './workouts.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button, Label, Form } from "./App.js";
+import Workouts from "./workouts.js";
 
 class View extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            workouts:[]
-        };
-    }
-    
-    Generate = (clicked) => {
-        if(clicked === true){
-            var addition = [];
-            Workouts.map((workout) => {
-                addition.push(workout);
-            });
-            this.setState({workouts: addition});
-        }
-        else{
-            return(<br></br>);
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      workouts: [],
+      numExercises: 0,
+      typeExercise: "Upper Body"
+    };
+  }
 
-    render() {
-        return(
-            <div class="container">
-                <div>
-                    <Button text="Generate Workout" func={this.Generate}></Button>
-                    <div class="list">
-                        {this.state.workouts.map((workout) => (
-                            <Label key={workout.name} name={workout.name} link={workout.hyperlink} type={workout.type} targetmuscle={workout["target-muscle"]} difficulty={workout.difficulty}></Label>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        );
+  setNum = (num) => {
+    this.setState({ numExercises: num }, function(){
+      console.log(this.state.numExercises);
+    });
+  };
+
+  setType = (type) => {
+    this.setState({ typeExercise: type }, function(){
+      console.log(this.state.typeExercise);
+    });
+  };
+
+  Generate = clicked => {
+    if (clicked === true) {
+      var addition = [];
+      var len = Workouts.length;
+      console.log("Length of Workouts Array: ", len);
+      var rndIndex = 0;
+      var count = parseInt(this.state.numExercises);
+      while(count > 0){
+        rndIndex = Math.floor(Math.random() * len);
+        console.log(rndIndex);
+        if(Workouts[rndIndex].type === this.state.typeExercise && !addition.includes(Workouts[rndIndex])){
+          count--;
+          addition.push(Workouts[rndIndex]);
+        }
+      }
+      console.log(addition);
+      this.setState({ workouts: addition });
+    } else {
+      return <br></br>;
     }
+  };
+
+  render() {
+    return (
+      <div class="container">
+        <div>
+          <Button text="Generate Workout" func={this.Generate}></Button>
+          <Form numFunc={this.setNum} typeFunc={this.setType}></Form>
+          <div class="list">
+            {this.state.workouts.map(workout => (
+              <Label
+                key={workout.name}
+                name={workout.name}
+                link={workout.hyperlink}
+                type={workout.type}
+                targetmuscle={workout["target-muscle"]}
+                difficulty={workout.difficulty}
+              ></Label>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<View />, document.getElementById('root'));
+ReactDOM.render(<View />, document.getElementById("root"));
